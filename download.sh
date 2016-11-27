3c72a0db540feef7677fb54ad17b4e9b653941fe
Bert Raeymaekers <bert.raeymaekers@schilduil.org>
2016-11-28 00:12:08 +0100

    Added a download script that downloads extra css and js files.

---
+++ download.sh
@@ -0,0 +1,16 @@
+
+mkdir -p ./targets/localweb/css/bootstrap/3.3.5
+cd targets/localweb/css/bootstrap/3.3.5/
+wget http://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css
+cd ../../../../../
+
+mkdir -p targets/localweb/js/jquery/2.1.4
+cd targets/localweb/js/jquery/2.1.4/
+wget https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+cd ../../../../../
+
+mkdir -p targets/localweb/js/bootstrap/3.3.5
+cd targets/localweb/js/bootstrap/3.3.5/
+wget http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js
+cd ../../../../../
+
