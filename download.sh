8bc02d6b6a5143713d1a231afc7b3bff5c258149
Bert Raeymaekers <bert.raeymaekers@schilduil.org>
2017-01-06 19:20:16 +0100

    Fixed .gitignore and download.sh with the refactored path.

--- download.sh
+++ download.sh
@@ -4,44 +4,44 @@
 # without internet access.
 
 # bootstrap
-mkdir -p targets/localweb/css/bootstrap/3.3.5
-cd targets/localweb/css/bootstrap/3.3.5/
+mkdir -p suapp/targets/localweb/css/bootstrap/3.3.5
+cd suapp/targets/localweb/css/bootstrap/3.3.5/
 wget http://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css
-cd ../../../../../
+cd ../../../../../../
 
 # bootstrap
-mkdir -p targets/localweb/js/bootstrap/3.3.5
-cd targets/localweb/js/bootstrap/3.3.5/
+mkdir -p suapp/targets/localweb/js/bootstrap/3.3.5
+cd suapp/targets/localweb/js/bootstrap/3.3.5/
 wget http://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.js
-cd ../../../../../
+cd ../../../../../../
 
 # html5shiv
-mkdir -p targets/localweb/js/html5shiv/3.7.0
-cd targets/localweb/js/html5shiv/3.7.0
+mkdir -p suapp/targets/localweb/js/html5shiv/3.7.0
+cd suapp/targets/localweb/js/html5shiv/3.7.0
 wget https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js
-cd ../../../../../
+cd ../../../../../../
 
 # respond.js
-mkdir -p targets/localweb/js/respond.js/1.3.0
-cd targets/localweb/js/respond.js/1.3.0
+mkdir -p suapp/targets/localweb/js/respond.js/1.3.0
+cd suapp/targets/localweb/js/respond.js/1.3.0
 wget https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js
-cd ../../../../../
+cd ../../../../../../
 
 # jquery 2.1.4 - actively used version
-mkdir -p targets/localweb/js/jquery/2.1.4
-cd targets/localweb/js/jquery/2.1.4/
+mkdir -p suapp/targets/localweb/js/jquery/2.1.4
+cd suapp/targets/localweb/js/jquery/2.1.4/
 wget https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
-cd ../../../../../
+cd ../../../../../../
 
 # jquery 3.1.1 - latest version (unused)
-mkdir -p targets/localweb/js/jquery/3.1.1
-cd targets/localweb/js/jquery/3.1.1
+mkdir -p suapp/targets/localweb/js/jquery/3.1.1
+cd suapp/targets/localweb/js/jquery/3.1.1
 wget https://code.jquery.com/jquery-3.1.1.min.js
 mv jquery-3.1.1.min.js jquery.min.js
-cd ../../../../../
+cd ../../../../../../
 
 # viz - graphviz in the browser
-mkdir -p targets/localweb/js/viz.js/1.4.0
-cd targets/localweb/js/viz.js/1.4.0
+mkdir -p suapp/targets/localweb/js/viz.js/1.4.0
+cd suapp/targets/localweb/js/viz.js/1.4.0
 wget https://github.com/mdaines/viz.js/releases/download/v1.4.0/viz.js
-cd ../../../../../
+cd ../../../../../../
