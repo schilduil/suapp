66376680a6f4fa78bee788a7588a1bda99567a34
Bert Raeymaekers <bert.raeymaekers@schilduil.org>
2017-11-08 22:02:33 +0100

    Better test coverage for simple_json.py & __init__.py.

--- suapp/targets/localweb/simple_json.py
+++ suapp/targets/localweb/simple_json.py
@@ -24,7 +24,7 @@ def to_json(object_to_serialize):
             result[column] = getattr(object_to_serialize, column)
         # Also putting out the primary key
         result['_pk_'] = getattr(object_to_serialize, '_pk_')
-        result['__str__'] = "%s" % (object_to_serialize)
+        #result['__str__'] = "%s" % (object_to_serialize)
         return result
     try:
         return json.dumps(object_to_serialize)
@@ -41,27 +41,3 @@ def to_json(object_to_serialize):
 def dumps(object_to_serialize, **kwargs):
     kwargs['default'] = to_json
     return json.dumps(object_to_serialize, **kwargs)
-
-
-if __name__ == "__main__":
-
-    class Something():
-
-        def __init__(self, name):
-            self.name = name
-
-        def toJSON(self):
-            return {"name": self.name}
-
-    class OtherThing():
-        def toJSON(self):
-            return "OtherThing %s" % (hex(self.__hash__()))
-
-    x = {
-        0: Something("zero"),
-        1: Something("one"),
-        2: Something("two")
-    }
-
-    print(dumps(x, indent=4))
-    print(dumps(OtherThing(), indent=4))
--- tests/targets/localweb/test_simple_json.py
+++ tests/targets/localweb/test_simple_json.py
@@ -6,30 +6,63 @@ import json
 import os
 import sys
 
-sys.path.append(os.path.join(os.getcwd(), 'suapp/targets/localweb'))
-import simple_json
+sys.path.append(os.getcwd())
+import suapp.orm
+import suapp.targets.localweb.simple_json as simple_json
+
+
+class Attribute():
+    def __init__(self, name):
+        self.name = name
+
+
+class UnSerializableSomething():
+    pass
 
 
 class Something():
 
     def __init__(self, name):
         self.name = name
+        self._attrs_ = [Attribute("name")]
+        self._columns_ = ["name"]
+        self._pk_ = self.name
 
     def toJSON(self):
         return {"name": self.name}
 
 
+class OrmSomething(suapp.orm.UiOrmObject):
+
+    def __init__(self, name):
+        self._ui_orm = Something(name)
+        self.ui_init()
+
+
 x = {
-            0: Something("zero"),
-            1: Something("one"),
-            2: Something("two")
+        0: Something("zero"),
+        1: Something("one"),
+        2: Something("two")
 }
 
 x_analog = {
-            0: {'name': 'zero'},
-            1: {'name': 'one'},
-            2: {'name': 'two'}
+        0: {'name': 'zero'},
+        1: {'name': 'one'},
+        2: {'name': 'two'}
 }
+
+x_orm = {
+        0: OrmSomething("zero"),
+        1: OrmSomething("one"),
+        2: OrmSomething("two")
+}
+
+x_orm_analog = {
+        0: {'_pk_': 'zero', 'name': 'zero'},
+        1: {'_pk_': 'one', 'name': 'one'},
+        2: {'_pk_': 'two', 'name': 'two'}
+}
+
 
 
 def test_dict_with_objects():
@@ -37,3 +70,13 @@ def test_dict_with_objects():
 
 def test_dict_with_objects_indented():
     assert simple_json.dumps(x, indent=4, sort_keys=True) == json.dumps(x_analog, indent=4, sort_keys=True)
+
+def test_dict_with_orm_objects():
+    assert simple_json.dumps(x_orm, sort_keys=True) == json.dumps(x_orm_analog, sort_keys=True)
+
+def test_dict_with_orm_objects_indented():
+    assert simple_json.dumps(x_orm, indent=4, sort_keys=True) == json.dumps(x_orm_analog, indent=4, sort_keys=True)
+
+def test_no_serializable():
+    with pytest.raises(TypeError):
+        simple_json.dumps(UnSerializableSomething())
