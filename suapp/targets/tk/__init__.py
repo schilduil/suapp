a313f573627287facf26d72fb1a03543f7def252
Bert Raeymaekers <bert.raeymaekers@schilduil.org>
2018-09-02 16:09:18 +0200

    First step in getting tk target up to the localweb level. Now it runs.

--- suapp/targets/tk/__init__.py
+++ suapp/targets/tk/__init__.py
@@ -118,7 +118,7 @@ class ToplevelWooster(Toplevel, suapp.jandw.Wooster):
         self.closed = True
         # Calling parent destroy() to actually destroy it.
         self.preClose()
-        super(Toplevel, self).destroy()
+        super().destroy()
 
     @loguse
     def close(self):
@@ -128,15 +128,76 @@ class ToplevelWooster(Toplevel, suapp.jandw.Wooster):
         self.destroy()
 
 
-ipsum = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pretium sapien sit amet magna viverra id faucibus nibh condimentum. Fusce dui magna, venenatis vel elementum eu, suscipit at quam. Nullam sed felis lorem, vel pretium leo. Cras at neque orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque ullamcorper quam eu ligula mattis tempor. Aliquam erat volutpat. Vivamus feugiat aliquam purus, euismod blandit lacus scelerisque vel. Phasellus sodales dignissim eros nec accumsan. Aenean est lectus, placerat sed ornare ut, congue nec nulla. Phasellus at sapien purus. Maecenas et sapien at ipsum fringilla tempus.
+ipsum = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Suspendisse pretium sapien sit amet magna viverra id faucibus nibh condimentum. \
+Fusce dui magna, venenatis vel elementum eu, suscipit at quam. Nullam sed felis lorem, vel pretium leo. \
+Cras at neque orci. \
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. \
+Pellentesque ullamcorper quam eu ligula mattis tempor. \
+Aliquam erat volutpat. Vivamus feugiat aliquam purus, \
+euismod blandit lacus scelerisque vel. \
+Phasellus sodales dignissim eros nec accumsan. \
+Aenean est lectus, placerat sed ornare ut, congue nec nulla. \
+Phasellus at sapien purus. \
+Maecenas et sapien at ipsum fringilla tempus.
 
-Donec non quam mauris. Vivamus bibendum ante id velit rhoncus molestie. Nullam bibendum suscipit elit sed aliquam. Maecenas felis magna, laoreet ut volutpat eget, varius at lorem. Quisque nisl dolor, aliquet et vestibulum nec, ullamcorper quis libero. Aliquam erat volutpat. Aenean mauris augue, varius sit amet iaculis quis, rhoncus at eros. Phasellus luctus congue cursus. Praesent lobortis vestibulum cursus. Nulla euismod nisi ac diam semper molestie. Curabitur quis leo at velit ultrices consectetur in in tellus. Nunc auctor erat ac justo porttitor consectetur. Cras vestibulum elit nec nulla consectetur euismod. Curabitur sodales vehicula lacus quis iaculis. Nulla laoreet eros eget ipsum commodo aliquet.
+Donec non quam mauris. \
+Vivamus bibendum ante id velit rhoncus molestie. \
+Nullam bibendum suscipit elit sed aliquam. \
+Maecenas felis magna, laoreet ut volutpat eget, varius at lorem. \
+Quisque nisl dolor, aliquet et vestibulum nec, ullamcorper quis libero. \
+Aliquam erat volutpat. \
+Aenean mauris augue, varius sit amet iaculis quis, rhoncus at eros. \
+Phasellus luctus congue cursus. \
+Praesent lobortis vestibulum cursus. \
+Nulla euismod nisi ac diam semper molestie. \
+Curabitur quis leo at velit ultrices consectetur in in tellus. \
+Nunc auctor erat ac justo porttitor consectetur. \
+Cras vestibulum elit nec nulla consectetur euismod. \
+Curabitur sodales vehicula lacus quis iaculis. \
+Nulla laoreet eros eget ipsum commodo aliquet.
 
-Cras gravida egestas nunc a hendrerit. Quisque consequat arcu ac dolor bibendum a porttitor augue malesuada. Etiam suscipit augue ac eros lobortis auctor. Vivamus urna sem, ultrices non ultrices in, sodales id justo. Aenean gravida bibendum risus, a pharetra arcu rhoncus quis. Maecenas interdum fringilla dapibus. Sed non diam massa, a sodales eros. Cras interdum nibh leo. Praesent non urna turpis. Pellentesque posuere massa vel ligula porta sed egestas arcu lacinia. Curabitur rutrum ultrices vestibulum. Mauris posuere, arcu id tempor volutpat, lacus nisl euismod felis, non facilisis purus sem ac sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris posuere ornare elit non congue. Praesent pretium, nisl eget pulvinar pulvinar, nibh purus sollicitudin tellus, eget malesuada lacus nulla vel neque.
+Cras gravida egestas nunc a hendrerit. \
+Quisque consequat arcu ac dolor bibendum a porttitor augue malesuada. \
+Etiam suscipit augue ac eros lobortis auctor. \
+Vivamus urna sem, ultrices non ultrices in, sodales id justo. \
+Aenean gravida bibendum risus, a pharetra arcu rhoncus quis. \
+Maecenas interdum fringilla dapibus. \
+Sed non diam massa, a sodales eros. \
+Cras interdum nibh leo. \
+Praesent non urna turpis. \
+Pellentesque posuere massa vel ligula porta sed egestas arcu lacinia. \
+Curabitur rutrum ultrices vestibulum. \
+Mauris posuere, arcu id tempor volutpat, lacus nisl euismod felis, non facilisis purus sem ac sem. \
+Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. \
+Mauris posuere ornare elit non congue. \
+Praesent pretium, nisl eget pulvinar pulvinar, nibh purus sollicitudin tellus, eget malesuada lacus nulla vel neque.
 
-In non elit nisi. Phasellus porta vestibulum erat non condimentum. Sed sed augue neque, sed venenatis eros. In et nunc neque. Mauris mi velit, dignissim id vehicula at, semper vitae est. Ut porta lacus eget ipsum faucibus vel luctus metus scelerisque. Duis ipsum nulla, rutrum sit amet ultricies et, sodales vitae sem. Donec suscipit aliquam diam, id mollis nibh gravida vel. Quisque in blandit lectus. Fusce interdum nulla turpis, sit amet venenatis metus. Praesent ac tristique tortor.
+In non elit nisi. \
+Phasellus porta vestibulum erat non condimentum. \
+Sed sed augue neque, sed venenatis eros. \
+In et nunc neque. \
+Mauris mi velit, dignissim id vehicula at, semper vitae est. \
+Ut porta lacus eget ipsum faucibus vel luctus metus scelerisque. \
+Duis ipsum nulla, rutrum sit amet ultricies et, sodales vitae sem. \
+Donec suscipit aliquam diam, id mollis nibh gravida vel. \
+Quisque in blandit lectus. \
+Fusce interdum nulla turpis, sit amet venenatis metus. \
+Praesent ac tristique tortor.
 
-Suspendisse ut magna sed nulla cursus rutrum. Suspendisse in lacus dui, non bibendum tellus. Nulla facilisi. Morbi urna justo, volutpat eu mattis id, facilisis sed sapien. Vestibulum vitae sapien orci. Proin aliquet euismod neque, vel sodales nulla hendrerit vitae. Sed auctor euismod adipiscing. Curabitur feugiat dui quis lorem gravida a mattis eros vehicula. Aenean eget ullamcorper nunc. Mauris sagittis eros aliquam dolor varius sit amet eleifend dolor placerat. Vestibulum nec dui metus. Nullam nec ante in lectus faucibus condimentum. Donec ac enim purus, et bibendum erat. Aliquam vestibulum tempus consectetur.'''
+Suspendisse ut magna sed nulla cursus rutrum. \
+Suspendisse in lacus dui, non bibendum tellus. Nulla facilisi. \
+Morbi urna justo, volutpat eu mattis id, facilisis sed sapien. \
+Vestibulum vitae sapien orci. \
+Proin aliquet euismod neque, vel sodales nulla hendrerit vitae. \
+Sed auctor euismod adipiscing. \
+Curabitur feugiat dui quis lorem gravida a mattis eros vehicula. \
+Aenean eget ullamcorper nunc. \
+Mauris sagittis eros aliquam dolor varius sit amet eleifend dolor placerat. \
+Vestibulum nec dui metus. \
+Nullam nec ante in lectus faucibus condimentum. \
+Donec ac enim purus, et bibendum erat. \
+Aliquam vestibulum tempus consectetur.'''
 
 
 class TableWindow(ToplevelWooster):
@@ -197,7 +258,7 @@ class TableWindow(ToplevelWooster):
         self.createWidgets()
 
 
-class TableFactory(suapp.jandw.Wooster):
+class Table(suapp.jandw.Wooster):
 
     @loguse
     def inflow(self, jeeves, drone):
@@ -334,7 +395,7 @@ class RecordWindow(ToplevelWooster):
         self.createWidgets()
 
 
-class RecordFactory(suapp.jandw.Wooster):
+class Record(suapp.jandw.Wooster):
     '''
     Note: it will open a new window. If you want to avoid doubles open, use subclass UniqueRecordFactory
     '''
@@ -349,7 +410,7 @@ class RecordFactory(suapp.jandw.Wooster):
             RecordWindow().inflow(jeeves, drone)
 
 
-class UniqueRecordFactory(RecordFactory):
+class UniqueRecord(Record):
 
     windowreferences = {}
 
@@ -526,7 +587,7 @@ class Application(Frame, suapp.jandw.Wooster):
         When you click the [X]
         '''
         self.preClose()
-        super(Frame, self).destroy()
+        super().destroy()
 
     @loguse
     def preClose(self):
@@ -600,6 +661,207 @@ class Configuration(suapp.jandw.Wooster):
                 self.window = AboutFrame()
         self.window.inflow(jeeves, drone)
         self.window.focus_set()
+
+
+class View(suapp.jandw.Wooster):
+    """
+    Generic view page for showing.
+    """
+    @loguse('@')  # Not logging the return value.
+    def inflow(self, jeeves, drone):
+        """
+        Entry point for the view.
+        """
+        self.jeeves = jeeves
+
+        # Getting the flow (usually uppercase) and ref (lowercase) names.
+        flow_name = drone.name
+        ref_name = flow_name.lower()
+        name = ref_name.capitalize()
+
+        # Getting the view definition.
+        definition = jeeves.views.get(flow_name, {})
+
+        # Getting the session, params and preparing the scope.
+        session = drone.dataobject.get('session', {})
+        # Setting default dabase paging parameters for the query.
+        query_params = {"pagenum": 1, "pagesize": 5}
+        # Getting the http request params.
+        for param in drone.dataobject['params']:
+            query_params[param] = drone.dataobject['params'][param][0]
+        scope = {} # NOTUSED
+        # scope.update(jeeves.ormscope) # jeeves.ormscope is always empty.
+
+        # JS parameters
+        js_params = {}
+        if drone.dataobject:
+            js_params['query'] = "query/%(query)s?pagenum=%(pagenum)s&pagesize=%(pagesize)s"
+        result = []
+        js_params['service_url'] = jeeves.app.configuration['httpd']['service_url']
+
+        """"
+        # Title
+        title = definition.get('name', name)
+        if logging.getLogger(self.__module__).isEnabledFor(logging.DEBUG):
+            html.append('<!-- DEBUG title = %s -->' % (title))
+
+        def_tabs = definition.get('tabs', {0: {'title': ''}})
+        if logging.getLogger(self.__module__).isEnabledFor(logging.DEBUG):
+            html.append('<!-- DEBUG def_tabs = %s -->' % (def_tabs))
+
+        tabs = collections.OrderedDict()
+        tab_count = 0
+        if 'query' in def_tabs:
+            if logging.getLogger(self.__module__).isEnabledFor(logging.DEBUG):
+                html.append('<!-- DEBUG query = %s -->' % (def_tabs['query']))
+            tab_title = def_tabs.get('title', name)
+            tab_objects = self.jeeves.do_query(def_tabs['query'], params=query_params)
+            parameters = self.jeeves.pre_query(def_tabs['query'], params=query_params)[1]
+            parameters['query'] = def_tabs['query']
+            js_query_params = with_expanded_values(js_params, params=parameters)
+            js_query_params['view'] = "testview" # TODO
+            html.append(View.raw_js % (js_query_params))
+            # TESTVIEW
+            html.append('<table id="testview">')
+            html.append('</table>')
+            if logging.getLogger(self.__module__).isEnabledFor(logging.DEBUG):
+                html.append('<!-- DEBUG tab_objects = %s -->' % (tab_objects))
+            for tab in tab_objects:
+                if tab_title[0] == ".":
+                    tabs[tab_count] = (getattr(tab, tab_title[1:]), tab)
+                else:
+                    tabs[tab_count] = (tab_title, tab)
+                tab_count += 1
+        else:
+            # Loop over all integer keys and get out the titles.
+            for i in def_tabs:
+                # TODO: is this second element in the tuple correct?
+                tabs[i] = (def_tabs[i]['title'], def_tabs[i])
+                if logging.getLogger(self.__module__).isEnabledFor(logging.DEBUG):
+                    html.append('<!-- DEBUG tabs: %s -->' % (tabs))
+
+        # Tab headers
+        html.append('<ul class="nav nav-tabs">')
+        for i in sorted(tabs): # CHECKME: DO WE NEED SORTED HERE?
+            if i is 0:
+                html.append('\t<li class="active"><a data-toggle="tab" href="#tab%s">%s</a></li>' % (i, tabs[i][0]))
+            else:
+                html.append('\t<li><a data-toggle="tab" href="#tab%s">%s</a></li>' % (i, tabs[i][0]))
+        html.append('</ul>')
+
+        # Tabs
+        html.append('<div class="tab-content">')
+        for i in sorted(tabs):
+            if logging.getLogger(self.__module__).isEnabledFor(logging.DEBUG):
+                html.append('<!-- DEBUG tab: %s = %s -->' % (i, tabs[i]))
+            if i is 0:
+                html.append('\t<div id="tab%s" class="tab-pane fade in active">' % (i))
+            else:
+                html.append('\t<div id="tab%s" class="tab-pane fade">' % (i))
+            # Sections
+            sections = tabs[i][1].get('sections', definition.get('sections', {0: {'title': ''}}))
+            if logging.getLogger(self.__module__).isEnabledFor(logging.DEBUG):
+                html.append('<!-- DEBUG sections: %s -->' % (sections))
+            for s in sorted(sections.keys(), key=str):
+                if logging.getLogger(self.__module__).isEnabledFor(logging.DEBUG):
+                    html.append('<!-- DEBUG section: %s -->' % (s))
+                if not str(s).isdigit():
+                    continue
+                section_title = sections[s].get('title', '')
+                html.append('\t\t<div class="panel panel-default">')  # panel-primary <> panel-default ?
+                if section_title != tabs[i][0]:
+                    html.append('\t\t\t<div class="panel-heading">%s</div>' % (section_title))
+                html.append('\t\t\t<div class="panel-body" id="section%s_%s">' % (i, s))
+                # Lines
+                lines = sections[s].get('lines', tabs.get('lines', definition.get('sections', {0: {'title': ''}})))
+                # DEBUGGING
+                if logging.getLogger(self.__module__).isEnabledFor(logging.DEBUG):
+                    html.append('<!-- DEBUG lines = %s -->' % (lines))
+                line_objects = []
+                if 'query' in lines:
+                    line_objects = self.jeeves.do_query(lines['query'], params=query_params)
+                    query_template, parameters = self.jeeves.pre_query(lines['query'], params=query_params)
+                    parameters['query'] = lines['query']
+                    js_query_params = with_expanded_values(js_params, params=parameters)
+                    js_query_params['view'] = "section%s_%s" % (i, s)
+                    if 'elements' in lines:
+                        for e in sorted(lines['elements']):
+                            value = lines['elements'][e].get('value', '#')
+                            element_type = lines['elements'][e].get('type', 'label').lower()
+                            outmessage = lines['elements'][e].get('outmessage', '')
+                            if value[0] == ".":
+                                value = '\' + data["objects"][elementid]["' + value[1:] + '"] + \''
+                            html_element = "&nbsp;"
+                            if element_type == "button":
+                                # Button
+                                html_element = View._button_as_button(
+                                    value=value,
+                                    outmessage=outmessage
+                                )
+                            else:
+                                # Label
+                                pass
+                            js_query_params['html'] = html_element
+                    html.append(View.raw_js % (js_query_params))
+                else:
+                    for line_object in line_objects:
+                        if logging.getLogger(self.__module__).isEnabledFor(logging.DEBUG):
+                            html.append('<!-- DEBUG line_object = %s (%s in %s) -->' % (line_object, type(line_object), line_object.__module__))
+                        line_elements = []
+                        # Line elements
+                        if 'elements' in lines:
+                            for e in sorted(lines['elements']):
+                                if logging.getLogger(self.__module__).isEnabledFor(logging.DEBUG):
+                                    html.append('<!-- DEBUG element = %s -->' % (e))
+                                value = lines['elements'][e].get('value', '#')
+                                element_type = lines['elements'][e].get('type', 'label').lower()
+                                outmessage = lines['elements'][e].get('outmessage', '')
+                                if value[0] == ".":
+                                    value = getattr(line_object, value[1:])
+                                if element_type == "button":
+                                    html.append("\t\t\t\t" +
+                                        View.button(
+                                            value=value,
+                                            outmessage=outmessage,
+                                            module=line_object.__module__,
+                                            table=line_object.__class__.__name__.split("_")[-1],
+                                            key=line_object._pk_
+                                        )
+                                    )
+                                else:
+                                    html.append("\t\t\t\t" + View.label(value=value))
+                    for l in sorted(lines.keys(), key=str):
+                        if str(l).isdigit():
+                            # Line elements
+                            if 'elements' in lines[l]:
+                                for e in sorted(lines[l]['elements']):
+                                    if logging.getLogger(self.__module__).isEnabledFor(logging.DEBUG):
+                                        html.append('<!-- DEBUG element = %s -->' % (e))
+                                    value = lines[l]['elements'][e].get('value', '#')
+                                    l_type = lines[l]['elements'][e].get('type', 'button').lower()
+                                    outmessage = lines[l]['elements'][e].get('outmessage', '')
+                                    if value[0] == ".":
+                                        value = getattr(line_object, value[1:])
+                                    if element_type == "button":
+                                        html.append("\t\t\t" +
+                                            View.button(
+                                                value=value,
+                                                outmessage=outmessage,
+                                                module=line_object.__module__,
+                                                table=line_object.__class__.__name__.split("_")[-1],
+                                                key=line_object._pk_
+                                            )
+                                        )
+                                    else:
+                                        html.append("\t\t\t" + View.label(value=value))
+                html.append('\t\t\t</div>')
+                html.append('\t\t</div>')
+
+            html.append('\t</div>')
+        html.append('</div>')
+
+        return (title, "\n".join(html))
+        """
 
 
 if __name__ == "__main__":
